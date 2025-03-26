dc82eb8cd88371fa42e415eba0655b0c5abc2ef0
Mazavi09 <vishneva09@gmail.com>
2025-03-26 15:55:48 +0300

    Добавьте файлы проекта.

---
+++ Лабораторная 1 МДК 01.04/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Лабораторная_1_МДК_01._04;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ Лабораторная 1 МДК 01.04/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.Win32;
+
+namespace TextEditor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void FontSize_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            if (ПТ.SelectedItem is ComboBoxItem selectedItem)
+            {
+                txtEditor.FontSize = Convert.ToDouble(selectedItem.Content);
+            }
+        }
+        private void FontFamily_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            if (Arial.SelectedItem is ComboBoxItem selectedItem)
+            {
+                txtEditor.FontFamily = new FontFamily(selectedItem.Content.ToString());
+            }
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*"
+            };
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, txtEditor.Text);
+            }
+        }
+
+        private void ClearText_Click(object sender, RoutedEventArgs e)
+        {
+            txtEditor.Clear();
+        }
+
+        private void txtEditor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+    }
+}
